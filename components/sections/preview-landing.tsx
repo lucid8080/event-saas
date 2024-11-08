838bf7e22135cd2491a802b7f3c10835eae0c7d8
lucid8080 <35409952+lucid8080@users.noreply.github.com>
2024-11-07 20:29:06 -0500

    homepage

--- components/sections/preview-landing.tsx
+++ components/sections/preview-landing.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
-
+// This is a preview landing page for the app that shows a preview of the app on home page.
 export default function PreviewLanding() {
-  return (
-    <div className="pb-6 sm:pb-16">
+//   return (
+//    <div className="pb-6 sm:pb-16">
       <MaxWidthWrapper>
         <div className="rounded-xl md:bg-muted/30 md:p-3.5 md:ring-1 md:ring-inset md:ring-border">
           <div className="relative aspect-video overflow-hidden rounded-xl border md:rounded-lg">
@@ -16,9 +16,9 @@ export default function PreviewLanding() {
               height={1000}
               priority={true}
             />
-          </div>
-        </div>
-      </MaxWidthWrapper>
-    </div>
-  );
-}
+           </div>
+         </div>
+       </MaxWidthWrapper>
+//     </div>
+//   );
+ }
